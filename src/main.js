0e410c7b1d2039172e8f0fa008df3cdf37bdac8e
lucas.chen (陳明審) <lucas.chen@gigabyte.com>
2025-05-04 18:11:58 +0800

    fix no css bugs

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'uno.css';
-import { BrowserRouter } from 'react-router-dom';
-createRoot(document.getElementById('root')).render(_jsx(BrowserRouter, { children: _jsx(App, {}) }));
